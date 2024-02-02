4bd30cab471c6266f26e3cdfad3911e9c4d6de15
Ravindra <ravindra@Ravindras-MacBook-Air.local>
2024-02-02 22:05:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
+import axios from "axios";
 
 function App() {
+  const baseURL = "https://jsonplaceholder.typicode.com/posts";
+  useEffect(() => {
+    // axios.get(`${baseURL}/101`).then((response) => {
+    //   console.log('check response.data', response.data);
+    // });
+  },[]);
+
+  const [name, setName] = useState(''); 
+  const [score, setScore] = useState(''); 
+  const [time, setTime] = useState(''); 
+  let intervalId = useRef(null);
+
+  const handleInput = (data) => {
+    const checkName = data.target.name;
+    if(checkName === 'name'){
+      setName(data.target.value);
+    } else if(checkName === 'score'){
+      setScore(data.target.value);
+    } else {
+      setTime(data.target.value);
+    }
+  }
+
+  const submitFn = () => {
+      const data = {
+        "userId": name,
+        "title": time,
+        "body": 'kuch na'
+      };
+      axios
+      .post(baseURL, data)
+      .then((response) => {
+        console.log('----------', response.data);
+      });
+  }
+
+
+  const startFn = () => {
+    const sec = 1;
+    intervalId.current = setInterval(() => {
+      setTime(sec++);
+    },1000);
+  }
+
+  const endFn = () => {
+    clearInterval(intervalId.current);
+    console.log('time ===', time);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Student Form</h1>
+      <div>
+        <label>Name</label>
+        <input type="text" name="name" onChange={(event) => handleInput(event)} />
+      </div>
+      <div>
+        <label>Score</label>
+        <input type="text" name="score" onChange={(event) => handleInput(event)}/>
+      </div>
+      <div>
+        <label>Time Taken</label>
+        <input type="text" name="time" onChange={(event) => handleInput(event)}/>
+      </div>
+      <div>
+        <button type="button" onClick={() => submitFn()}>Submit</button>
+      </div>
+      <div>
+        <button type="button" onClick={() => startFn()}>Start {time}</button>
+      </div>
+      <div>
+        <button type="button" onClick={() => endFn()}>End</button>
+      </div>
     </div>
   );
 }
